0ebb2fae9de929c6c10dc7d356cedcc2f57acc3c
Hugh Brown <hughdbrown@yahoo.com>
2023-08-11 19:35:28 -0600

    Convert to Prng struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,17 @@
-use rand::prelude::*;
-
 use fast_exponentiation::{
     fast_exp_mod,
 };
+use prng::{
+    Prng,
+};
 
-// Return a pseudorandom value in the range [min, max).
-fn next_u64(prng: &mut ThreadRng, min: u64, max: u64) -> u64 {
-    let range = (max - min) as u64;
-    min + prng.next_u64() % range
-}
 
 pub fn is_probably_prime(prime_cand: u32, tests: i32) -> bool {
-    let mut rng = thread_rng();
+    let prng = &mut Prng::new();
     let power: u32 = prime_cand - 1;
     println!("-----\nTesting {prime_cand}");
     for _ in 0..tests {
-        let n: u64 = next_u64(&mut rng, 1, prime_cand as u64);
+        let n: u64 = prng.next_u64(1, prime_cand as u64);
         println!("n = {n}");
         let r = fast_exp_mod(n as i64, power as i64, prime_cand as i64);
         if r != 1 {return false;}
@@ -24,14 +20,15 @@ pub fn is_probably_prime(prime_cand: u32, tests: i32) -> bool {
     true
 }
 
-pub fn find_prime(prng: &mut ThreadRng, min: u64, max: u64, tests: i32) -> u64 {
+pub fn find_prime(prng: &mut Prng, min: u64, max: u64, tests: i32) -> u64 {
    loop {
-       let n: u64 = next_u64(prng, min, max);
+       let n: u64 = prng.next_u64(min, max);
        if is_probably_prime(n as u32, tests) {
            return n;
        }
    }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -93,10 +90,10 @@ mod tests {
 
     #[test]
     fn find_prime_7() {
-        let mut rng = thread_rng();
+        let prng = &mut Prng::new();
         let min: u64 = 10_u64.pow(6);
         let max: u64 = 10_u64 * min - 1;
-        let p = find_prime(&mut rng, min, max, 25);
+        let p = find_prime(prng, min, max, 25);
 
         let factors = find_factors(p as usize);
         assert!(factors.len() == 1);
@@ -104,10 +101,10 @@ mod tests {
 
     #[test]
     fn find_prime_8() {
-        let mut rng = thread_rng();
+        let prng = &mut Prng::new();
         let min: u64 = 10_u64.pow(7);
         let max: u64 = 10_u64 * min - 1;
-        let p = find_prime(&mut rng, min, max, 25);
+        let p = find_prime(prng, min, max, 25);
 
         let factors = find_factors(p as usize);
         assert!(factors.len() == 1);
@@ -115,10 +112,10 @@ mod tests {
 
     #[test]
     fn find_prime_9() {
-        let mut rng = thread_rng();
+        let prng = &mut Prng::new();
         let min: u64 = 10_u64.pow(8);
         let max: u64 = 10_u64 * min - 1;
-        let p = find_prime(&mut rng, min, max, 25);
+        let p = find_prime(prng, min, max, 25);
 
         let factors = find_factors(p as usize);
         assert!(factors.len() == 1);
@@ -129,10 +126,10 @@ mod tests {
     //         at /Users/hughbrown/.cargo/git/checkouts/fast-exponentiation-5cd944c4894559d4/785f8d7/src/lib.rs:24:13
     //#[test]
     //fn find_prime_10() {
-    //    let mut rng = thread_rng();
+    //    let prng = &mut Prng::new();
     //    let min: u64 = 10_u64.pow(9);
     //    let max: u64 = 10_u64 * min - 1;
-    //    let p = find_prime(&mut rng, min, max, 25);
+    //    let p = find_prime(prng, min, max, 25);
     //
     //    let factors = find_factors(p as usize);
     //    let msg = format!("{} has factors: {:?}", p, factors);
